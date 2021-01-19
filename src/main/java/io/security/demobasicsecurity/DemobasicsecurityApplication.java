fe21cefeb4717f71c738ff143f1cc5aeeb70c933
문준 <mj.song@ubivelox.com>
2021-01-19 17:30:19 +0900

    최초등록

---
+++ src/main/java/io/security/demobasicsecurity/DemobasicsecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.demobasicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobasicsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemobasicsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/demobasicsecurity/DemobasicsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.demobasicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobasicsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
