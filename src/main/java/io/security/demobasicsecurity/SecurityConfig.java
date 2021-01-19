54e6a6c9c132e0866bcf35cdc2fe65d99ff25413
문준 <mj.song@ubivelox.com>
2021-01-19 17:40:59 +0900

    깃 테스트2

--- src/main/java/io/security/demobasicsecurity/SecurityConfig.java
+++ src/main/java/io/security/demobasicsecurity/SecurityConfig.java
@@ -29,7 +29,6 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  // 깃 테스트
   @Autowired
   UserDetailsService userDetailsService;
 
