ad60569fb4f94f9789d9a2c6b65165780f4209d1
SongMunJun <song960530@naver.com>
2021-02-21 20:09:17 +0900

    Note 클래스 수정

--- src/main/java/io/security/demobasicsecurity/Note.java
+++ src/main/java/io/security/demobasicsecurity/Note.java
@@ -1,10 +1,7 @@
 package io.security.demobasicsecurity;
 
 
-import io.security.demobasicsecurity.인증저장소_SecurityContextHolder_SecurityContext.SecurityConfig;
-
-public class Note
-{
+public class Note {
 
     // 필터 초기화 및 다중 보안 설정 하는 방법
     io.security.demobasicsecurity.필터초기화및다중보안설정.SecurityConfig SecurityConfig8 = new io.security.demobasicsecurity.필터초기화및다중보안설정.SecurityConfig();
@@ -13,7 +10,7 @@ public class Note
     io.security.demobasicsecurity.인증개념이해_Authentication.SecurityConfig SecurityConfig9 = new io.security.demobasicsecurity.인증개념이해_Authentication.SecurityConfig();
 
     // 인증 저장소 (SecurityContextHolder, SecurityContext)
-    io.security.demobasicsecurity.인증저장소_SecurityContextHolder_SecurityContext.SecurityConfig SecurityConfig10 = new SecurityConfig();
+    io.security.demobasicsecurity.인증저장소_SecurityContextHolder_SecurityContext.SecurityConfig SecurityConfig10 = new io.security.demobasicsecurity.인증저장소_SecurityContextHolder_SecurityContext.SecurityConfig();
 
     // 인증저장소필터_SecurityContextPersistenceFilter
     io.security.demobasicsecurity.인증저장소필터_SecurityContextPersistenceFilter.SecurityConfig SecurityConfig11 = new io.security.demobasicsecurity.인증저장소필터_SecurityContextPersistenceFilter.SecurityConfig();
